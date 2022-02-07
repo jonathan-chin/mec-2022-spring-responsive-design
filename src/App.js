41f9e0f154df91b30173021d33aa5862d87945d2
Jon Chin <jon@sharemeals.org>
2022-02-07 12:50:45 -0500

    initial demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@mui/material/Grid';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+	<Grid container spacing={4} id='container'>
+	    <Grid item xs={12} md={6}>
+		<div className='item'>
+		</div>
+	    </Grid>
+	    <Grid item xs={12} md={3}>
+		<div className='item'>
+		</div>
+	    </Grid>
+	    <Grid item xs={12} md={3}>
+		<div className='item'>
+		</div>
+	    </Grid>
+	    <Grid item xs={6} md={8}>
+		<div className='item'>
+		</div>
+	    </Grid>
+	    <Grid item xs={6} md={4}>
+		<div className='item'>
+		</div>
+	    </Grid>
+	    <Grid item xs={12} md={12}>
+		<div className='item'>
+		</div>
+	    </Grid>
+	</Grid>
+    );
 }
 
 export default App;
